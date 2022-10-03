ef6cb4eef23629aaf11a5f1589dc56f29653d35f
gaelLara21 <gaellara19@gmail.com>
2022-10-03 11:07:44 -0500

    Commit2

--- js/app.js
+++ js/app.js
@@ -4,17 +4,35 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
+
+let ListadoCarrito = [];
+
+
 const agregarCurso = (e) => {
     //Capturar el evento
     e.preventDefault();
-    console.log(e.target.classlist);
-    //e.target.classlist.contains('agregar-carrito')
-    if (e.target.classlist.contains('agregar carrito')){
-        console.log('Presionaaste el boton: ');
+    if (e.target.classList.contains('agregar-carrito')){
+        const curso = e.target.parentElement.parentElement;
+        const infoCurso= {
+            imagen: curso.querySelector('img').src,
+            nombre: curso.querySelector('h4').textContend,
+            precio: curso.querySelector('p.precio').textContend,
+            id: curso.querySelector('.agregar-carrito').getAttribute('data-id'),
+            cantidad: 1
+        }
+    agregarCarrito(infoCurso);
+    
     }
+
 }
 
+    //Función agregar carrito
+    const agregarCarrito = curso =>{
+        ListadoCarrito = [...ListadoCarrito, curso]
+        console.log(ListadoCarrito);
+    }
 
+    //e.target.classlist.contains('agregar-carrito')
 const cargarEventListener = ()=> {
 
     //Agregar finción de carga de cursos al carrito
